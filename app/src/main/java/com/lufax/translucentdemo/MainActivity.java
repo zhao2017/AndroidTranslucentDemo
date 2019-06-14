7df7b4080d3d6a17f712524078656bdeb5e0274a
ex-zhaohang002 <ex-zhaohang002@lu.com>
2019-06-14 17:08:21 +0800

    ---------

--- app/src/main/java/com/lufax/translucentdemo/MainActivity.java
+++ app/src/main/java/com/lufax/translucentdemo/MainActivity.java
@@ -1,21 +1,28 @@
 package com.lufax.translucentdemo;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.ActionBar;
 
 import com.lufax.translucentdemo.base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.iv_img)
+    ImageView ivImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 //        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         if (Build.VERSION.SDK_INT >= 21) {
             View decorView = getWindow().getDecorView();
---
+++ app/src/main/java/com/lufax/translucentdemo/ui/SeaActivity.java
@@ -0,0 +1,13 @@
+package com.lufax.translucentdemo.ui;
+
+import android.os.Bundle;
+
+import com.lufax.translucentdemo.base.BaseActivity;
+
+public class SeaActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
